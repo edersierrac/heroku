251e52f37b996e1e555c269071c1e7bc5d904c08
edersierrac <93484406+edersierrac@users.noreply.github.com>
2021-11-02 17:29:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+<?php 
+    include("conexion.php");
+    $con=conectar();
 
+    $sql="SELECT *  FROM alumno";
+    $query=mysqli_query($con,$sql);
+
+    $row=mysqli_fetch_array($query);
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
