f34c7d637b5416c17b5423e1b40ba4281630930c
eder sierra <edersc2016@gmail.com>
2021-11-02 17:48:37 -0500

    cambio final

---
+++ index2.php
@@ -0,0 +1,22 @@
+<html> 
+ <head> 
+   <title>Nuestro primer ejemplo en PHP 7</title> 
+ </head> 
+ 
+  <body>
+ 
+  <?php
+ 
+ echo "¡Hola mundo! <br>";
+ echo "¡prueba de cambio! <br>";
+ echo "¡prueba de cambio2! <br>";
+ 
+ 
+ for($i = 0; $i < 10; $i++)
+  {
+   echo "Línea número: ".$i."<br>";
+  }
+ ?>
+ 
+  </body> 
+  </html>
