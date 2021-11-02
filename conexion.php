aefe3ea0f54fe2a7fe88e3e0090af7516270f59b
edersierrac <93484406+edersierrac@users.noreply.github.com>
2021-11-02 17:29:52 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+function conectar(){
+    $host="dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user="rmykfjr3nx827wv2";
+    $pass="u64c9hvm0qt12xx4";
+
+    $bd="mhnynkr8etzq2vys";
+
+    $con=mysqli_connect($host,$user,$pass);
+
+    mysqli_select_db($con,$bd);
+
+    return $con;
+}
+?>
