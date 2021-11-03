3996c7843b1c23c31072125d57fa465fca850b33
eder sierra <edersc2016@gmail.com>
2021-11-03 18:29:00 -0500

    cambio

--- index.php
+++ index.php
@@ -57,8 +57,8 @@
                                                 <th><?php  echo $row['dni']?></th>
                                                 <th><?php  echo $row['nombres']?></th>
                                                 <th><?php  echo $row['apellidos']?></th>    
-                                                <!--<th><a href="actualizar.php?id=<?php echo $row['cod_estudiante'] ?>" class="btn btn-info">Editar</a></th>-->
-                                                <!--<th><a href="delete.php?id=<?php echo $row['cod_estudiante'] ?>" class="btn btn-danger">Eliminar</a></th> -->                                       
+                                                <th><a href="actualizar.php?id=<?php echo $row['cod_estudiante'] ?>" class="btn btn-info">Editar</a></th>-->
+                                                <th><a href="delete.php?id=<?php echo $row['cod_estudiante'] ?>" class="btn btn-danger">Eliminar</a></th> -->                                       
                                             </tr>
                                         <?php 
                                             }
